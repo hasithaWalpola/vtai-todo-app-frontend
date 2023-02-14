73f1ff319f4eaf8b47dddb2caeb1d45a29936f2d
hasithaWalpola <walpolahasitha@gmail.com>
2023-02-14 16:53:10 +0530

    chore: change environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
+require('dotenv').config({
+  path: 'src/environments/.env'
+});
+
 export const environment = {
   api_base_url: 'https://vtai-backend-dot-todo-backend-376718.uc.r.appspot.com/api/v1/',
-  api_key: '',
+  api_key: process.env['API_KEY'],
   production: false,
   appVersion: '',
 };
