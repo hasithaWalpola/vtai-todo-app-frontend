ad8562a9060667719fb38dccacf63776c6b10ec0
hasithaWalpola <walpolahasitha@gmail.com>
2023-02-14 17:19:32 +0530

    fix: cicd ts command issue fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,7 @@
-require('dotenv').config({
-  path: 'src/environments/.env'
-});
 
 export const environment = {
   api_base_url: 'https://vtai-backend-dot-todo-backend-376718.uc.r.appspot.com/api/v1/',
-  api_key: process.env['API_KEY'],
+  api_key: '',
   production: false,
   appVersion: '',
 };
