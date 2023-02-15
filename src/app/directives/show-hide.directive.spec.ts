a807563a6d0b2741419053b54994a48f3799bcff
hasithaWalpola <walpolahasitha@gmail.com>
2023-02-15 15:45:46 +0530

    chore(testing): directive test cases

--- src/app/directives/show-hide.directive.spec.ts
+++ src/app/directives/show-hide.directive.spec.ts
@@ -1,8 +1,45 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { ShowHideDirective } from './show-hide.directive';
+import { AuthService } from '../services/shared/auth.service';
+
+@Component({
+  template: `
+    <div appShowHide></div>
+  `
+})
+class TestComponent { }
 
 describe('ShowHideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowHideDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let authService: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ShowHideDirective, TestComponent],
+      providers: [AuthService],
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+    authService = TestBed.inject(AuthService);
+  });
+
+  it('should hide the element if user role is 1', () => {
+    spyOn(authService, 'getLoggedUser').and.returnValue({ role: 1 });
+
+    fixture.detectChanges();
+
+    const div: DebugElement = fixture.debugElement.query(By.directive(ShowHideDirective));
+    expect(div.nativeElement.style.display).toBe('none');
+  });
+
+  it('should show the element if user role is not 1', () => {
+    spyOn(authService, 'getLoggedUser').and.returnValue({ role: 2 });
+
+    fixture.detectChanges();
+
+    const div: DebugElement = fixture.debugElement.query(By.directive(ShowHideDirective));
+    expect(div.nativeElement.style.display).toBe('contents');
   });
 });
